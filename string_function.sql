9ad1df5ef7f6bac6f8449f1b278c3d2abe5de1c9
Vaishnavi Shinde <132147643+vaish1919@users.noreply.github.com>
2024-12-09 17:35:25 +0530

    Add files via upload

---
+++ string_function.sql
@@ -0,0 +1,11 @@
+select *from actor;
+select first_name, lower(first_name) from actor;
+
+select first_name , length(first_name) from actor;
+
+
+-- dummy table ( row 1 column )
+select 1+2 from dual;
+
+select length ("hey") from dual;
+
